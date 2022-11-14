32ff4278061b0a8339a4d11c59d8d73e502ed789
Abiodun001-world <adekunleabiodun354@gmail.com>
2022-11-14 01:49:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## A responsive portfolio web app for a  Tennis Star 
+## Build with with Html and css and adding new feature on it.
---
+++ index.js
@@ -0,0 +1,2 @@
+// background: url('https://scontent.fiba1-1.fna.fbcdn.net/v/t1.6435-9/121162750_131353845383816_3048756711348959122_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=TKwUPaoq0kgAX8pl6mK&_nc_ht=scontent.fiba1-1.fna&oh=00_AfB9D0IRZAdwhUgTXvydOHHrE-zzn-HG1h8ORx5EN-MRBw&oe=638CB67F');
+const paragraph = document.querySelector('.paragraph-page');
