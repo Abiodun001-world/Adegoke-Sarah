48436698adb3233077cbe8ac38949132a017f183
Abiodun001-wold <adekunleabiodun354@gmail.com>
2023-07-30 13:56:43 -0400

    working on this project

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let video = document.querySelector("sarahVideo");
+// In your JavaScript, enable the audio of the video
+let video = document.getElementById("sarahVideo");
+
+// This will play the audio of the video
+video.muted = false;
+
 
