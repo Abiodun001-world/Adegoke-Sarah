c9b75d04337df1b9d3b6ce83410555b9cf672919
Abiodun Adekunle <94719889+Abiodun001-world@users.noreply.github.com>
2025-01-21 09:58:47 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// In your JavaScript, enable the audio of the video
 let video = document.getElementById("sarahVideo");
 
 // This will play the audio of the video
 video.muted = false;
 
-
+// This will play the video
+video.play();
